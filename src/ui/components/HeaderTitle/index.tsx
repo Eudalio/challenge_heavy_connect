b64815c18eed3d6be90617538e214be332c56512
Eudalio Sousa <eudalio.sousa@allya.com.br>
2022-03-01 17:41:50 -0300

    feat: headerTitle component added

---
+++ src/ui/components/HeaderTitle/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container, HeaderText} from './styles';
+
+const HeaderTitle: React.FC = () => {
+	return (
+		<Container>
+			<HeaderText>Tractor Inspection</HeaderText>
+		</Container>
+	);
+};
+
+export default HeaderTitle;
---
+++ src/ui/components/HeaderTitle/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	height: 50%;
+	display: flex;
+	justify-content: flex-end;
+`;
+
+export const HeaderText = styled.Text`
+	color: #fff;
+	font-size: 30px;
+	letter-spacing: 0.6px;
+	font-weight: 600;
+`;
