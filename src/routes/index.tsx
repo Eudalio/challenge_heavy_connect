1025c5a00c7835e67426bf62e7d278248038f928
Eudalio Sousa <eudalio.sousa@allya.com.br>
2022-03-01 17:38:08 -0300

    feat: theme and routes configured

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import TractorsList from '../ui/pages/TractorsList';
+
+const {Navigator, Screen} = createNativeStackNavigator();
+
+const Routes = () => {
+	return (
+		<NavigationContainer>
+			<Navigator screenOptions={{headerShown: false}}>
+				<Screen name="Home" component={TractorsList} />
+			</Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default Routes;
---
+++ src/ui/themes/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+	header: '#1A57B9',
+	background: '#E3E2E2',
+	goodStatus: '#8ccc87',
+	repairStatus: '#fcca57',
+	brokenStatus: '#F95D57',
+	btnColor: '#52C782',
+	gray: '#8893A0',
+	white: '#fff',
+};
+
+export default colors;
