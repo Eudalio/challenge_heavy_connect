06df4227efca38bf4c3dd6aced2950c1eed92518
Eudalio Sousa <eudalio.sousa@allya.com.br>
2022-03-03 10:00:01 -0300

    chore: updated colors palette

--- src/ui/themes/colors.ts
+++ src/ui/themes/colors.ts
@@ -1,11 +1,13 @@
 const colors = {
 	header: '#1A57B9',
+	backgroundIcon: '#4179c7',
 	background: '#E3E2E2',
 	goodStatus: '#8ccc87',
 	repairStatus: '#fcca57',
 	brokenStatus: '#F95D57',
 	btnColor: '#52C782',
 	gray: '#8893A0',
+	placeholderInput: '#86919FDE',
 	white: '#fff',
 };
 
