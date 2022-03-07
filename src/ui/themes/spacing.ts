b5fa2360d118178065c7e64ed6566cfd25e7a08c
Eudalio Sousa <eudalio.sousa@allya.com.br>
2022-03-07 08:36:01 -0300

    chore: updated spacing default

--- src/ui/themes/spacing.ts
+++ src/ui/themes/spacing.ts
@@ -3,7 +3,7 @@ import {Dimensions} from 'react-native';
 const {width, height} = Dimensions.get('window');
 
 const spacing = {
-	default: 20,
+	default: 15,
 	screenWidth: width,
 	screenHeight: height,
 };
